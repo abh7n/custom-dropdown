24ec6a7b2b7eab68746b99cd78df439e8797439e
Abhin <abhinprakash699@gmail.com>
2024-10-25 00:04:51 +0530

    removed 'open' property

--- script.js
+++ script.js
@@ -6,24 +6,21 @@ const arrow = document.querySelector('.arrow');
 
 dropdownButton.addEventListener('click', () => {
   dropdownList.classList.toggle('show');
-  dropdownButton.classList.toggle('open');
   arrow.textContent = dropdownButton.classList.contains('open') ? '▲' : '▲';
 });
 
 document.addEventListener('click', (e) => {
   if (!e.target.closest('.dropdown')) {
     dropdownList.classList.remove('show');
-    dropdownButton.classList.remove('open');
     arrow.textContent = '▲';
   }
-});
+});     
 
 dropdownItems.forEach((item) => {
   item.addEventListener('click', () => {
     dropdownButton.textContent = item.textContent + ' ';
     dropdownList.classList.remove('show');
-    dropdownButton.classList.remove('open');
-    arrow.textContent = '▲';
+    
     dropdownItems.forEach((item) => item.classList.remove('selected'));
     item.classList.add('selected');
   });
